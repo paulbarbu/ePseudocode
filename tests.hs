57e62f9389ac509f81cd655dd8675ea0606fad85
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2014-09-15 14:34:03 +0300

    test ideas

---
+++ tests.hs
@@ -0,0 +1,31 @@
+-- assign function call or expression or function def
+-- return fucntion call or expression or funciton def
+-- sequence of stements in complete if in simple if, in for, while, function def
+
+
+runLex mainParser "func x() ret 5 sf func daca 3 atunci 5 sf daca"
+runLex mainParser "func x() ret func y() 1 sffunc daca 1 atunci sfdaca sffunc"
+runLex mainParser "func x() ret func y() 1 sffunc ret 1 sffunc"
+
+runLex mainParser "func x() daca 5 atunci 3 sfdaca daca 4 atunci 5 sfdaca sffunc"
+runLex mainParser "func x() ret x() sf func"
+
+runLex mainParser "daca 5 atunci daca 2 atunci 3 sfdaca daca 3 atunci 3 sfdaca sfdaca"
+runLex mainParser "daca 5 atunci daca 2 atunci 3 sfdaca sfdaca"
+runLex mainParser "daca 5 atunci 3 sfdaca"
+
+runLex mainParser "daca 5 atunci daca 2 atunci 3 sfdaca daca 3 atunci 3 sfdaca sfdaca"
+
+runLex mainParser "func x() daca 5 atunci 3 sf daca ret 5 sf func"
+runLex mainParser "func x() daca 5 atunci 3 sf daca sffunc daca 5 atunci 3 sfdaca"
+
+
+runLex mainParser "func x() daca 5 atunci 3 sfdaca sffunc"
+
+runLex mainParser "func x() daca 5 atunci 3 sfdaca ret 5 sffunc"
+
+runLex mainParser "func x() ret func y() 1 sffunc sffunc"
+
+runLex mainParser "a=func x() ret x() sf func"
+runLex mainParser "func x() ret 3 sf func"
+runLex mainParser "a= func x() ret x() sf func"
